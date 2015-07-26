343864008166bf245705e649279e251d95697cb6
pcubillos <pcubillos@knights.ucf.edu>
2015-07-26 19:38:21 -0400

    Cleaned the setup.py file. Bumped MC3 revison (Now version 2.0.14).

--- src/cfuncs/setup.py
+++ src/cfuncs/setup.py
@@ -4,24 +4,30 @@ from distutils.core import setup, Extension
 
 srcdir = 'src/'      # C-code source folder
 incdir = 'include/'  # Include filder with header files
-#libdir = 'lib/'      # Where the shared objects are put
 
 files = os.listdir(srcdir)
 # This will filter the results for just the c files:
 files = list(filter(lambda x:     re.search('.+[.]c$',     x), files))
 files = list(filter(lambda x: not re.search('[.#].+[.]c$', x), files))
 
-ext_mod = []
 inc = [get_include(), incdir]
+eca = []  # ['-fopenmp']
+ela = []  # ['-lgomp']
 
+extensions = []
 for i in range(len(files)):
-  exec("mod{:d} = Extension('{:s}', sources=['{:s}{:s}'], include_dirs=inc, "
-       "extra_compile_args=['-fopenmp'], extra_link_args=['-lgomp'])".format(
-       i, files[i].rstrip('.c'), srcdir, files[i]))
+  e = Extension(files[i].rstrip(".c"),
+                sources=["{:s}{:s}".format(srcdir,files[i])],
+                include_dirs=inc,
+                extra_compile_args=eca,
+                extra_link_args=ela)
+  extensions.append(e)
 
-  exec('ext_mod.append(mod{:d})'.format(i))
 
-setup(name="MC3",
-      version='1.0.0',
-      description='c extension functions',
-      ext_modules = ext_mod)
+setup(name         = "MC3 C-extensions",
+      version      = "1.0.0",
+      author       = "Patricio Cubillos",
+      author_email = "pcubillos@fulbrightmail.org",
+      url          = "https://github.com/pcubillos/MCcubed",
+      description  = "MC3 C-extension functions",
+      ext_modules  = extensions)
