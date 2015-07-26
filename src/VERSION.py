8f549ded0ebaba2f75fdbb5304e981c6e4bdf6ca
pcubillos <pcubillos@knights.ucf.edu>
2015-07-26 12:26:13 -0400

    Bumped MC3 revision (now version 2.0.10).

--- src/VERSION.py
+++ src/VERSION.py
@@ -1,4 +1,4 @@
 # MC3 Version:
-MC3_VER = 2  # Major version
-MC3_MIN = 0  # Minor version
-MC3_REV = 9  # Revision
+MC3_VER = 2   # Major version
+MC3_MIN = 0   # Minor version
+MC3_REV = 10  # Revision
