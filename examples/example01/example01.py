b00b3bfadc11acb40f2ac507e61464d0fb6e596a
Patricio Cubillos <pcubillos@knights.ucf.edu>
2016-01-07 17:51:46 +0100

    Updated example01.py Moved the example quadratic function to its own folder. MC3 version 2.0.32.

--- examples/example01/example01.py
+++ examples/example01/example01.py
@@ -1,158 +1,62 @@
-# This script shows how to run MCMC from an interactive python sesion, 
-# for a quadratic function.
+# This script shows how to run MCMC from an interactive python sesion.
 
-# This script assumes that your current folder is /examples/example01/
+# Preamble
+# --------
+# To correctly execute this script, one needs to set the correct paths
+# to the source code.  The paths are given as if the Python session
+# runs from the MCcubed/examples/tutorial01/ folder of the MCcubed repository.
 
-# ::::: Running from an interactive python session ::::::::::::::::::
+# Alternatively, edit the paths from this script to adjust to your
+# working directory.
 
-# The module mccubed.py (multi-core MCMC) wraps around mcmc.py to enable
-# mutiprocessor capacity (using MPI), and use of configuration files.
 
-import sys
+# Import the necessary modules:
+import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
-sys.path.append("../../src")
-import mccubed as mc3
-import mcplots as mp
-import mcutils as mu
 
-# Get function to model/sample.
+sys.path.append("../../src/")
+import mcmc as mcmc
+import chain as ch
+import mcplots as mp
+sys.path.append("../models/")
 from quadratic import quad
 
 # Create a synthetic dataset:
-x  = np.linspace(0, 10, 100)  # Independent model variable
-p0 = 3, -2.4, 0.5             # True-underlying model parameters
-y  = quad(p0, x)              # Noiseless model
+x  = np.linspace(0, 10, 5000)         # Independent model variable
+p0 = [3, -2.4, 0.5]                   # True-underlying model parameters
+y  = quad(p0, x)                      # Noiseless model
 uncert = np.sqrt(np.abs(y))           # Data points uncertainty
 error  = np.random.normal(0, uncert)  # Noise for the data
 data   = y + error                    # Noisy data set
 
-# Set the MCMC arguments:
-# -----------------------
-help(mc3.mcmc)  # Displays the MCMC function docstring.
-
-# Define the function to model the data in the MCMC: As a requirement, the
-# first argument of func must be the set of fitting parameters.
+indparams = [x]
 
 # Define as callable:
 func = quad
-# Or by function name and module name:
-#     func = ("quad", "quadratic")
-# If the module is out of the python-path scope, set it as the 3rd parameter:
-#     func = ("quad", "quadratic", "path/to/file")
 
 
-# We will fit the quad polynomial coefficients:
-params   = np.array([ 20.0,  -2.0,   0.1]) # Initial guess of fitting params.
-pmin     = np.array([-10.0, -20.0, -10.0]) # Boundaries
-pmax     = np.array([ 40.0,  20.0,  10.0])
-# stepsize is used as the standard deviation of a Gaussian function:
-# For Metropolis Random Walk, the Gaussian function draws the parameter
-# proposals.  For Differential Evolution, the Gaussian function draws the
-# starting values of the chains about the initial guess. Later, is used in
-# the support distribution as a Gaussian with standard deviation
-# of 0.01*stepsize.
-stepsize = np.array([  1.0,   0.5,   0.1])
+walk = "demc"   # Choose between: {'demc' or 'mrw'}
+nchains = 10    # Number of parallel chains
+nsamples = 1e5  # Number of MCMC samples to compute
+thinning = 2    # Thinning factor for outputs
+burnin = 900    # Number of burned-in samples per chain
 
-# indparams packs every additional parameter of func, each argument is an
-# item in the indparams list:
-indparams = [x]
-# If func does not require additional arguments define indparams as:
-# indparams=[], or simple leave it undefined in the mcmc call.
 
-# MCMC setup:
-numit   = 3e4
-nchains = 10
-walk    = 'demc'
-grtest  = True
-burnin  = 100
-plots   = True
-savefile  = 'output_ex1.npy'
-savemodel = 'output_model.npy'
+params = [4, -2, 0.2]
+stepsize = [0.03, 0.03, 0.005]
+# Parameter prior probability distributions:
+pmin = [-10.0, -10.0, -10.0]
 
+
+reload(ch)
+reload(mcmc)
+reload(mp)
 # Run the MCMC:
-allp, bp = mc3.mcmc(data, uncert, func, indparams,
-            params, pmin, pmax, stepsize,
-            numit=numit, nchains=nchains, walk=walk, grtest=grtest,
-            burnin=burnin, plots=plots, savefile=savefile, savemodel=savemodel)
-
-
-# Evaluate and plot:
-y0 = quad(params, x)  # Initial guess values
-y1 = quad(bp,     x)  # MCMC best fitting values
-
-plt.figure(10)
-plt.clf()
-plt.plot(x, y, "-k",   label='true')
-plt.errorbar(x, data, yerr=uncert, fmt=".b", label='data')
-plt.plot(x, y0, "-g",  label='Initial guess')
-plt.plot(x, y1, "-r",  label='MCMC best fit')
-plt.legend(loc="best")
-plt.xlabel("X")
-plt.ylabel("quad(x)")
-
-# The module mcplots provides helpful plotting functions:
-# Plot trace plot:
-parname = ["constant", "linear", "quadratic"]
-mp.trace(allp, title="Fitting-parameter Trace Plots", parname=parname)
-
-# Plot pairwise posteriors:
-mp.pairwise(allp, title="Pairwise posteriors", parname=parname)
-
-# Plot marginal posterior histograms:
-mp.histogram(allp, title="Marginal posterior histograms", parname=parname)
-
-
-# ::::: Multi-core Markov-chain Monte Carlo :::::::::::::::::::::::::
-# A multi-process MCMC will use one CPU for each MCMC-chain
-# to calculate the model for the set of parameters in that chain.
-# To use MPI set the mpi argument to True, and run mc3.mcmc as usual:
-mpi=True
-allp, bp = mc3.mcmc(data, uncert, func, indparams,
-            params, pmin, pmax, stepsize,
-            numit=numit, nchains=nchains, walk=walk, grtest=grtest,
-            burnin=burnin, plots=plots, savefile=savefile, mpi=mpi)
-
-
-# ::::::: Arguments as files ::::::::::::::::::::::::::::::::::::::::
-# As said in the help description, the data, uncert, indparams, params, 
-# pmin, pmax, stepsize, prior, priorlow, and priorup arrays can be
-# read from a text file.  In this case, set the argument to be the file name.
-
-# Each line in the 'indparams' file must contain one element of the indparams
-# list, the values separated by (one or more) empty spaces.
-# The other files must contain one array value per line (i.e., column-wise). 
-
-# Furthermore, the 'data' file can also contain the uncert array (as a second
-# column, values separated by a empty space).
-# Likewise, the 'params' file can contain the pmin, pmax, stepsize, prior,
-# priorlow, and priorup arrays (as many or as few, provided that they are
-# written in columns in that precise order).
-
-# The mcutils module provides the function 'writedata' to easily make these
-# files in the required format, for example:
-mu.writebin([data, uncert],                  'data_ex01.dat')
-mu.writedata([params, pmin, pmax, stepsize], 'pars_ex01.dat')
-mu.writebin(indparams,                       'indp_ex01.dat')
-# Check them out.  These files can contain empty or comment lines without
-# interfering with the routine.
-
-# Set the arguments to the file names:
-data      = 'data_ex01.dat'
-params    = 'pars_ex01.dat'
-indparams = 'indp_ex01.dat'
-# Run MCMC:
-allp, bp = mc3.mcmc(data=data, func=func, indparams=indparams,
-                    params=params,
-                    numit=numit, nchains=nchains, walk=walk, grtest=grtest,
-                    burnin=burnin, plots=plots, savefile=savefile)
-
-
-# ::::: Configuration files :::::::::::::::::::::::::::::::::::::::::
-# MC3 Also supports the use of a configuration file to set up the mcmc
-# arguments.  Use the cfile argument of mc3.mcmc to specify a configuration
-# file:
-cfile = "my_config_file.cfg"
-# Check out example02 to see a configuration file example.
-# In case of conflict, when an argument is specified twice, the
-# inline-command value will override the configuration-file value.
+Z, Zchain, bp = mcmc.mcmc(data, uncert, func,  indparams=indparams,
+   params=params,  pmin=pmin, pmax=None, stepsize=stepsize,
+   prior=None,   priorlow=None,    priorup=None,
+   nsamples=nsamples,  nchains=nchains,  walk=walk, wlike=False,
+   leastsq=True, chisqscale=True, grtest=True,  burnin=burnin,
+   thinning=thinning, hsize=1, kickoff='normal',
+   plots=True,  savefile='theoutput', savemodel=None, resume=False)
