0d213b8eb899fe5a0dddef79bf54da7137ca856b
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-11-29 12:24:47 +0100

    Added CRlo, CRhi, uncertp, and meanp to the output savefile.

--- MCcubed/mc/mcmc.py
+++ MCcubed/mc/mcmc.py
@@ -627,7 +627,8 @@ def mcmc(data,         uncert=None,   func=None,      indparams=[],
 
   # Save definitive results:
   if savefile is not None:
-    np.savez(savefile, bestp=bestp, Z=Z, Zchain=Zchain, Zchisq=Zchisq)
+    np.savez(savefile, bestp=bestp, Z=Z, Zchain=Zchain, Zchisq=Zchisq,
+             CRlo=CRlo, CRhi=CRhi, uncertp=uncertp, meanp=meanp)
   if savemodel is not None:
     np.save(savemodel, allmodel)
 
