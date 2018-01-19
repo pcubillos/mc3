8aab12c672428dfffce0eee3b97b26b5fa5fb7b1
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-01-19 12:06:16 +0100

    Updated copyright year to 2018.

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MC3'
-copyright = u'2015-2017, Patricio Cubillos'
+copyright = u'2015-2018, Patricio Cubillos'
 author = u'Patricio Cubillos'
 
 # The version info for the project you're documenting, acts as replacement for
