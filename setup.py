f1c6ea3044eebe8f2c48a9b982410b08ee4a3049
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2019-07-10 18:07:33 +0200

    Cleaned up setup.py. Added ffast-math extra compile arg, added long description, and include package dara argument.

--- setup.py
+++ setup.py
@@ -1,41 +1,41 @@
+# Copyright (c) 2015-2019 Patricio Cubillos and contributors.
+# MC3 is open-source software under the MIT license (see LICENSE).
+
 import os
-import sys
 import re
 import setuptools
-from numpy import get_include
 from setuptools import setup, Extension
 
-topdir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(topdir + "/MCcubed")
-import VERSION as ver
+from numpy import get_include
+
+import MCcubed as mc3
 
 
-version = "{:d}.{:d}.{:d}".format(ver.MC3_VER, ver.MC3_MIN, ver.MC3_REV)
+srcdir = 'src_c/'          # C-code source folder
+incdir = 'src_c/include/'  # Include folder with header files
 
-srcdir = topdir + '/src_c/'          # C-code source folder
-incdir = topdir + '/src_c/include/'  # Include filder with header files
-
-files = os.listdir(srcdir)
-# This will filter the results for just the c files:
-files = list(filter(lambda x:     re.search('.+[.]c$',     x), files))
-files = list(filter(lambda x: not re.search('[.#].+[.]c$', x), files))
+cfiles = os.listdir(srcdir)
+cfiles = list(filter(lambda x:     re.search('.+[.]c$',     x), cfiles))
+cfiles = list(filter(lambda x: not re.search('[.#].+[.]c$', x), cfiles))
 
 inc = [get_include(), incdir]
-eca = []
+eca = ['-ffast-math']
 ela = []
 
 extensions = []
-for i in range(len(files)):
-    e = Extension("MCcubed.lib."+files[i].rstrip(".c"),
-                  sources=["{:s}{:s}".format(srcdir, files[i])],
-                  include_dirs=inc,
-                  extra_compile_args=eca,
-                  extra_link_args=ela)
+for cfile in cfiles:
+    e = Extension("MCcubed.lib." + cfile.rstrip(".c"),
+        sources=["{:s}{:s}".format(srcdir, cfile)],
+        include_dirs=inc,
+        extra_compile_args=eca,
+        extra_link_args=ela)
     extensions.append(e)
 
+with open('README.md', 'r') as f:
+    readme = f.read()
 
-setup(name         = "MCcubed",
-      version      = version,
+setup(name         = "mc3",
+      version      = mc3.__version__,
       author       = "Patricio Cubillos",
       author_email = "patricio.cubillos@oeaw.ac.at",
       url          = "https://github.com/pcubillos/MCcubed",
@@ -44,8 +44,11 @@ setup(name         = "MCcubed",
                           'scipy>=0.17.1',
                           'matplotlib>=2.2.3',],
       tests_require = ['pytest>=3.9',],
+      include_package_data=True,
       license      = "MIT",
       description  = "Multi-core Markov-chain Monte Carlo package.",
+      long_description=readme,
+      long_description_content_type="text/markdown",
       include_dirs = inc,
       entry_points={"console_scripts": ['mc3 = MCcubed.__main__:main']},
       ext_modules  = extensions)
