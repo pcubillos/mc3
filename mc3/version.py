83d009651f93dc0cdf4be508de31c4ea433ed20d
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2023-03-27 09:08:45 +0200

    Updated mc3 version to 3.1.0b2.

--- mc3/version.py
+++ mc3/version.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2015-2022 Patricio Cubillos and contributors.
+# Copyright (c) 2015-2023 Patricio Cubillos and contributors.
 # mc3 is open-source software under the MIT license (see LICENSE).
 
 # mc3 Version:
 MC3_VER = 3  # Major version
 MC3_MIN = 1  # Minor version
-MC3_REV = '0b1'  # Revision
+MC3_REV = '0b2'  # Revision
 
 __version__ = f'{MC3_VER}.{MC3_MIN}.{MC3_REV}'
