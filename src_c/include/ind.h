fe714f616f7f14f5217c2744499a119c7dcdda41
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-04-09 13:53:11 +0200

    Nope, did not work (and broke ubuntu). Undoing.

--- src_c/include/ind.h
+++ src_c/include/ind.h
@@ -3,7 +3,7 @@
 
 /* Definitions for indexing Numpy arrays:                                   */
 
-/* 1D double ndarray:                                                       */
-#define INDd(a,i) (char *)((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
-/* 1D integer ndarray:                                                      */
-#define INDi(a,i) (char *)((int *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+/* 1D double ndarray: */
+#define INDd(a,i) *((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+/* 1D integer ndarray: */
+#define INDi(a,i) *((int *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
