a1b0ece0cedb50aa89df677d60b1fd5f4df86554
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-04-09 14:43:13 +0200

    Testing sarah and henry changes to INDd and INDi. Added explicit install dependencies.

--- src_c/include/ind.h
+++ src_c/include/ind.h
@@ -4,6 +4,6 @@
 /* Definitions for indexing Numpy arrays:                                   */
 
 /* 1D double ndarray: */
-#define INDd(a,i) *((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+#define INDd(a,i) *(((double *)PyArray_DATA(a)) + i * PyArray_STRIDES(a)[0] / sizeof(double))
 /* 1D integer ndarray: */
-#define INDi(a,i) *((int *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+#define INDi(a,i) *(((int *)PyArray_DATA(a)) + i * PyArray_STRIDES(a)[0] / sizeof(int))
