560c5d37a3f90c1831b8858bafe9778c311ed827
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-04-09 13:50:31 +0200

    Wild guess for INDd fix.

--- src_c/include/ind.h
+++ src_c/include/ind.h
@@ -4,6 +4,6 @@
 /* Definitions for indexing Numpy arrays:                                   */
 
 /* 1D double ndarray:                                                       */
-#define INDd(a,i) *((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+#define INDd(a,i) (char *)((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
 /* 1D integer ndarray:                                                      */
-#define INDi(a,i) *((int    *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+#define INDi(a,i) (char *)((int *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
