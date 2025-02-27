3e4734b891fcf0687ba8e1080da748b417ac87d5
pcubillos <patricio.cubillos@oeaw.ac.at>
2025-02-27 13:30:07 +0100

    Updated copyright year

--- mc3/__init__.py
+++ mc3/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2023 Patricio Cubillos and contributors.
+# Copyright (c) 2015-2025 Patricio Cubillos and contributors.
 # mc3 is open-source software under the MIT license (see LICENSE).
 
 from .sampler_driver import *
